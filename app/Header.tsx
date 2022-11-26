20118edeadeb7236c78f04d4c882e982c6e1e7f2
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-11-26 23:39:31 +0900

    Headerを作成

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useUser } from '@auth0/nextjs-auth0';
+import Image from 'next/image';
 import styles from './page.module.css';
 
 export const Header = () => {
@@ -10,14 +11,24 @@ export const Header = () => {
   if (error) return <div>{error.message}</div>;
 
   console.log(user);
-  
 
   return (
     <div className={styles.header}>
       {!user ? (
         <a href="/api/auth/login">Login</a>
       ) : (
-        <a href="/api/auth/logout">Logout</a>
+        <>
+          <a href="/api/auth/logout">Logout</a>
+          {user.picture && (
+            <Image
+              src={user.picture}
+              alt="Avater"
+              width={32}
+              height={32}
+              className={styles.avatar}
+            />
+          )}
+        </>
       )}
     </div>
   );
